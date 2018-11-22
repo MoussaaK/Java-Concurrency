2dad82abfcfd3e38490a8a471e1b06f728a3c037
11408560 <11408560@f200-6.ig-edu.univ-paris13.fr>
2018-11-22 15:07:20 +0100

    Updated Main

--- src/main/java/org/konate/tp_concurrence_maven/serie1/exo0/Main.java
+++ src/main/java/org/konate/tp_concurrence_maven/serie1/exo0/Main.java
@@ -1,10 +1,21 @@
 package org.konate.tp_concurrence_maven.serie1.exo0;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Runnable task = () -> System.out.println(Thread.currentThread().getName());
+		
+		
+		Thread thread = new Thread(task);
+		thread.start();
+		
+		Callable<Object> callable = Executors.callable(task);		
+		try {
+			callable.call();
+		} catch (Exception e) {
+		}
 	}
-
 }
