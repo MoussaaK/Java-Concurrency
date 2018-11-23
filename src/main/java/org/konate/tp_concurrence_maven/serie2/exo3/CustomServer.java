7cbff080f7c6a4937420f6cdb42c2fbc664144fb
11408560 <11408560@f200-6.ad.ig-edu.univ-paris13.fr>
2018-11-23 15:59:33 +0100

    Created my custom server

--- src/main/java/org/konate/tp_concurrence_maven/serie2/exo3/CustomServer.java
+++ src/main/java/org/konate/tp_concurrence_maven/serie2/exo3/CustomServer.java
@@ -9,37 +9,58 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomServer {
 
-    public static void main(String[] args) throws IOException {
+	private static ServerSocket server;
 
-        ServerSocket server = new ServerSocket(8080);
+	public static void main(String[] args) throws IOException {
 
-        while (true) {
-            System.out.println("Listening to request");
-            Socket socket = server.accept();
-            System.out.println("Accepting request");
+		server = new ServerSocket(8080);
+		
+		int processors = Runtime.getRuntime().availableProcessors();
+		ExecutorService service = Executors.newFixedThreadPool(processors-2);
 
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		while (true) {
+			System.out.println("Listening to request");
+			Socket socket = server.accept();
+			System.out.println("Accepting request");
 
-            OutputStream outputStream = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
+			Callable<?> response = () -> {
+				InputStream inputStream = socket.getInputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String order = reader.readLine();
-            while (order != null) {
-                if (order.startsWith("GET")) {
-                    writer.printf("Received GET order : %s\n", order);
-                    writer.flush();
-                    System.out.printf("Received GET order : %s\n", order);
-                } else if (order.equals("bye")) {
-                    System.out.printf("Closing connection\n");
-                    socket.close();
-                }
-                order = reader.readLine();
-            }
-
-        }
-    }
+				OutputStream outputStream = socket.getOutputStream();
+				PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
+				String order = null;
+				try {
+					order = reader.readLine();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				while (order != null) {
+					if (order.startsWith("GET")) {
+						writer.printf("Received GET order : %s\n", order);
+						writer.flush();
+						System.out.printf("Received GET order : %s\n", order);
+					} else if (order.equals("bye")) {
+						System.out.printf("Closing connection\n");
+						socket.close();
+					}
+					
+					order = reader.readLine();
+					
+				}
+				return order;
+			};
+			
+			service.submit(response);
+			
+			
+		}
+		
+	}
 }
